ace62bfaa00dd968671423dc742f07d83863e48c
Sam <105581592+Exaeous@users.noreply.github.com>
2022-06-01 23:56:39 +0530

    First commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Samuel Pulukuri on 01/06/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
